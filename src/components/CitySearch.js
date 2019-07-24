e766cfa3003364a508f32157cfc9548838d28b55
Jonah Alexander-Awad <34581478+JonahAwad94@users.noreply.github.com>
2019-07-24 14:38:28 -0400

    City-Search Added

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,14 +1,41 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 
-class citySearch extends Component {
+class CitySearch extends Component {
     constructor(props){
-        super(props);
+      super(props);
+      this.state= {
+        test: [],
+        test2: []
+      }
     }
-    render() {
-        
+  
+    componentDidMount(){
+      this.test();
     }
-}
+  
+    test=()=>{
+      axios.get("http://ctp-zip-api.herokuapp.com/zip/10303")
+      .then(res => {
+        console.log(res);
+        this.setState({
+          test: res.data[0].City,
+          test2: res.data[0].Country
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    }
+    render(){
+      return (
+        <div>
+          {this.state.test};
+          {this.state.test2};
+        </div>
+      );
+    }
+  }
 
 
-export default citySearch;
+export default CitySearch;
