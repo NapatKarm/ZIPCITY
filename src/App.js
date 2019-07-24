da20aeb5f1fef3d5d1493ef41c945483375727c8
NapatKarm <napat.karm@gmail.com>
2019-07-24 13:39:00 -0400

    Fixed Files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state= {
+      test: []
+    }
+  }
+
+  componentDidMount(){
+    this.test();
+  }
+
+  test=()=>{
+    axios.get("http://ctp-zip-api.herokuapp.com/zip/10016")
+    .then(res => {
+      console.log(res);
+      this.setState({
+        test: res.data[0].Country
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+  render(){
+    return (
+      <div>
+
+        {this.state.test}
+      </div>
+    );
+  }
 }
 
 export default App;
