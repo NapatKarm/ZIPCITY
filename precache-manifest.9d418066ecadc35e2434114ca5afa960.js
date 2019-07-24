fb80b766ce7f1eb41f66386041ef13a01cf84b93
NapatKarm <napat.karm@gmail.com>
2019-07-24 11:54:08 -0400

    Updates

---
+++ precache-manifest.9d418066ecadc35e2434114ca5afa960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a4ec717ffb8089d2fb048ccd935011c",
+    "url": "/NapatKarm/ZIPCITY/index.html"
+  },
+  {
+    "revision": "ca26efa80424cd6eda99",
+    "url": "/NapatKarm/ZIPCITY/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/NapatKarm/ZIPCITY/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ca26efa80424cd6eda99",
+    "url": "/NapatKarm/ZIPCITY/static/js/main.386732ca.chunk.js"
+  },
+  {
+    "revision": "4c91637dbdcc45f3b606",
+    "url": "/NapatKarm/ZIPCITY/static/js/runtime~main.6b6a72d0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/NapatKarm/ZIPCITY/static/media/logo.5d5d9eef.svg"
+  }
+]);
