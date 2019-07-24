2750b828ef3814624e99bd138b96ac5af16b0b81
NapatKarm <napat.karm@gmail.com>
2019-07-24 13:54:31 -0400

    Fixed Files #3 Added base files

---
+++ src/components/CitySearch.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class citySearch extends Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        
+    }
+}
+
+
+export default citySearch;
---
+++ src/components/ZipSearch.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class zipSearch extends Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        
+    }
+}
+
+
+export default zipSearch.js;
