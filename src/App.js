0c074eb4a01bbe412ffe431691a5bb5b5efd7676
NapatKarm <napat.karm@gmail.com>
2019-07-25 12:14:59 -0400

    Finished ZipSearch, removed ZipShow, added ZipSearch.css, changed App.css and App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,51 @@ import ZipSearch from './components/ZipSearch.js'
 import CitySearch from './components/CitySearch.js'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      data : [],
+      zip : '',
+      zipcode : ''
+    }
+  }
+  
+  componentDidMount(){
+    this.searchUp();
+  }
+  searchUp=()=>{
+    axios.get(`http://ctp-zip-api.herokuapp.com/zip/${this.state.zip}`)
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        data : res.data,
+        zipcode : this.state.zip
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+  zipChange=(event)=>{
+    this.setState({zip: event.target.value});
+  }
+
   render(){
+   let zipResult = this.state.data.map((zipinfo)=>
+    <ZipSearch data = {zipinfo}/>
+   ) 
+
     return(
       <div>
-        <p>This is result of City Search</p>
-        <br></br>
-        <CitySearch/>
-        <br></br>
-        <p>This is result of Zip Search</p>
-        <br></br>
-        <ZipSearch/>
+      <h1 class= "headline">Current Zipcode: {this.state.zipcode}</h1>
+      <form  className = "form">
+          <label>
+              Enter Zipcode:
+              <input type="text" name="zipcode" value={this.state.zip} onChange={this.zipChange}/>
+          </label>
+      </form>
+      <button onClick={this.searchUp} class = "button">Submit</button>
+        {zipResult}
       </div>
     );
   }
--- src/components/ZipSearch.js
+++ src/components/ZipSearch.js
@@ -1,64 +1,35 @@
 import React, {Component} from 'react';
-import axios from 'axios';
+import './ZipSearch.css';
 
-function Searched(props){
-  return(
-    <div>
-      <p>This Actually Works! The City is {props.city} and wage is {props.wage}</p>
-    </div>
-  )
-}
 class ZipSearch extends Component {
     constructor(props){
       super(props);
-      this.state= {
-        City: [],
-        State: [],
-        Lat: [],
-        Long: [],
-        Population: [],
-        Wage: [],
-        zip: "",
-        zipcode: ""
-      }
-    }
-  
-    componentDidMount(){
-      this.searchUp();
-    }
-    searchUp=()=>{
-      this.setState({zipcode: this.state.zip})
-      axios.get(`http://ctp-zip-api.herokuapp.com/zip/${this.state.zipcode}`)
-      .then(res => {
-        this.setState({
-          city: res.data[3].City,
-          state: res.data[2].State,
-          lat: res.data[2].Lat,
-          long: res.data[2].Long,
-          population: res.data[2].EstimatedPopulation,
-          wage: res.data[3].TotalWages
-        })
-      })
-      .catch(err => {
-        console.log(err);
-      })
-    }
-
-    zipChange=(event)=>{
-      this.setState({zip: event.target.value});
     }
     render(){
+      var City = this.props.data.City;
+      var State = this.props.data.State;
+      var Long = this.props.data.Long;
+      var Lat = this.props.data.Lat;
+      var Pop = this.props.data.EstimatedPopulation;
+      var Wage = this.props.data.TotalWages;
+
       return (
         <div>
-          <p>Current Zipcode: {this.state.zipcode}</p>
-          <form  className = "form">
-              <label>
-                  Enter Zipcode:
-                  <input type="text" name="zipcode" value={this.state.zip} onChange={this.zipChange}/>
-              </label>
-          </form>
-          <button onClick={()=>this.searchUp()}>Submit</button>
-          <Searched city = {this.state.city} wage = {this.state.wage}/>
+          <table class = "table">
+            <th>{City}</th>
+        <tr>
+          <td><div>State : {State}</div></td>
+        </tr>
+        <tr>
+          <td><div>Location: ({Long},{Lat})</div></td>
+        </tr>
+        <tr>
+          <td><div>Population(Estimated): {Pop}</div></td>
+        </tr>
+        <tr>
+          <td><div>Total Wage: {Wage}</div></td>
+        </tr>
+          </table>
         </div>
       );
     }
