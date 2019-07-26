bfb4f8a0670e0a095aea8eaabd6768a290bf728a
Jonah Alexander-Awad <34581478+JonahAwad94@users.noreply.github.com>
2019-07-26 14:27:11 -0400

    City/Zip Search Functionality Complete

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -4,37 +4,19 @@ import axios from 'axios';
 class CitySearch extends Component {
     constructor(props){
       super(props);
-      this.state= {
-        test: [],
-        test2: []
-      }
     }
-  
-    componentDidMount(){
-      this.test();
+      render(){
+        var Zip = this.props.data;
+
+        return (
+          <div>
+            <table class = "table">
+              <th>{Zip}</th>
+            </table>
+          </div>
+        );
     }
-  
-    test=()=>{
-      axios.get("http://ctp-zip-api.herokuapp.com/zip/10303")
-      .then(res => {
-        console.log(res);
-        this.setState({
-          test: res.data[0].City,
-          test2: res.data[0].Country
-        })
-      })
-      .catch(err => {
-        console.log(err);
-      })
-    }
-    render(){
-      return (
-        <div>
-          {this.state.test};
-          {this.state.test2};
-        </div>
-      );
-    }
+    
   }
 
 
